780fa5c07e200984d0da6fc99ac18ed211294278
Ignacio Trujillo <+@ignac.es>
2017-10-10 14:33:12 -0300

    added no sandbox for puppeteer

--- api/validate.js
+++ api/validate.js
@@ -2,7 +2,8 @@ const puppeteer = require('puppeteer');
 
 async function run(username, serie, type) {
   const browser = await puppeteer.launch({
-    headless: true
+    headless: true,
+    args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
